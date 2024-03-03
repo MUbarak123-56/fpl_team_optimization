81254ece322b104b6fa23f2a3ebc9d02b7417c63
Farouk Haroun <farouk.o.haroun@gmail.com>
2024-03-03 11:17:07 -0600

    handle input

--- formation_selection.py
+++ formation_selection.py
@@ -1,7 +1,102 @@
 
 import pandas as pd
-
-bqm = dimod.BinaryQuadraticModel('BINARY')
-
-
+from pyqubo import Constraint, Array
+import neal
+from dwave.system import DWaveSampler, EmbeddingComposite, LeapHybridBQMSampler,  LeapHybridSampler
+import dimod
 data = pd.read_excel("data.xlsx")
+columns = ["name", "position", "value", "total_points"]
+data = data[columns]
+df_use = data.sort_values("position").reset_index(drop=True)
+for i in range(len(df_use)):
+    df_use.loc[i, "variable"] = "x[" + str(i) + "]"
+df_use
+columns = ["variable","name", "position", "value", "total_points"]
+total_points = df_use["total_points"].to_list()
+value = df_use["value"].to_list()
+df_use = df_use[columns]
+lagrange = 1716
+lagrange_budget = 1551
+num_var = 38
+slack_num = 1
+def get_input(prompt, min_val, max_val):
+    while True:
+        try:
+            value = int(input(prompt))
+            if min_val <= value <= max_val:
+                return value
+            else:
+                print(f"Please choose a number between {min_val} and {max_val}.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def handle_inputs():
+    defense = get_input("How many defenders do you want? Choose a number between 3 and 5: ", 3, 5)
+    midfield = get_input("How many midfielders do you want? Choose a number between 2 and 5: ", 2, 5)
+    forward = get_input("How many forwards do you want? Choose a number between 1 and 4: ", 1, 4)
+
+    return defense, midfield, forward
+
+defense, midfield, forward = handle_inputs()
+summation = defense + midfield + forward
+
+while summation != 10:
+    print("The total number of players must be 10. Please try again.")
+    defense, midfield, forward = handle_inputs()
+    summation = defense + midfield + forward
+
+print(f"Team configuration: {defense} Defenders, {midfield} Midfielders, {forward} Forwards")
+
+x = Array.create('x', shape=num_var, vartype='BINARY')
+s = Array.create('s', shape=slack_num, vartype='BINARY')
+# objective function
+h = sum(n * x for x, n in zip(x, total_points))
+C1 = lagrange * Constraint((sum(x[n] for n in range(0, num_var)) - 11)**2,
+                    label='11 players team')
+C2 = lagrange * Constraint((sum(x[n] for n in range(0, 10))-defense)**2,
+                    label=str(defense) + " defenders")
+C3 = lagrange * Constraint((sum(x[n] for n in range(10, 21))-forward)**2,
+                    label=str(forward) + " forwards")
+C4 = lagrange * Constraint((sum(x[n] for n in range(21, 28))-1)**2,
+                    label= "1 keeper")
+C5 = lagrange * Constraint((sum(x[n] for n in range(28,  38))-midfield)**2,
+                    label=str(midfield) + " midfielders")
+C6 = lagrange_budget * Constraint((sum(n * x for x, n in zip(x, value)) + s[0] -700)**2,
+                                  label="budget")
+H = -1 * h + C1 + C2 + C3 + C4 + C5 + C6
+model = H.compile()
+qubo, offset = model.to_qubo()
+bqm = model.to_bqm()
+
+# Uncomment to solve problem by simulation
+# sa = neal.SimulatedAnnealingSampler()
+# sampleset = sa.sample(bqm, num_reads=10000)
+
+# Solve problem with QPU
+api_token = 'DEV-257ed80ce0a221025ddaa4b7acb440d9978e1f42'
+sampler = LeapHybridSampler(token= api_token)
+sampleset = sampler.sample(bqm,
+                            label="FPL line-up optimization")
+# Decode samples and select the best one
+decoded_samples = model.decode_sampleset(sampleset)
+best_sample = min(decoded_samples, key=lambda x: x.energy)
+
+# Print to see if constraints are fulfilled
+print(best_sample.constraints())
+# Print results for best line-up
+lineup_df = pd.DataFrame(best_sample.sample.items())
+lineup_df.columns = ['variable', 'selected']
+lineup_df = lineup_df[(lineup_df['variable'].str.startswith(
+    'x', na=False)) & (lineup_df['selected'] == 1)]
+lineup_df = df_use.merge(lineup_df, on=['variable'])
+
+# Print line-up and maximized energy for the objective function
+gk = lineup_df[lineup_df["position"] == "GK"]
+defense_list = lineup_df[lineup_df["position"] == "DEF"]
+midfield_list = lineup_df[lineup_df["position"] == "MID"]
+forward_list = lineup_df[lineup_df["position"] == "FWD"]
+ordered_lineup_df = pd.concat([gk, defense_list, midfield_list, forward_list], axis=0).reset_index(drop=True)
+ordered_lineup_df = ordered_lineup_df[["name", "position", "value", "total_points"]]
+print(ordered_lineup_df)
+print("Total sum of points: ", ordered_lineup_df['total_points'].sum())
+print("Total budget: ", ordered_lineup_df['value'].sum())
