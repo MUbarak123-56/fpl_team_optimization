43900f9446d53da85b32e7c37f4ecc2c0b745571
“MUbarak123-56” <mubarak.a.ganiyu@vanderbilt.edu>
2024-03-12 16:31:24 -0500

    new

--- FPL_Lineup_⚽.py
+++ FPL_Lineup_⚽.py
@@ -7,13 +7,16 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 import time
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title = "FPL Line Up Optimizer", page_icon="⚽")
 
 st.header("FPL Line Up Optimizer ⚽")
 
 st.subheader("Welcome to the FPL Line Up Optimizer App")
-st.write("""The purpose of this app is to assist FPL fans to select the optimal starting line-up according to data collected from Gameweek 1 to Gameweek 23 of the 2023/24 season. 
+st.write("""The purpose of this app is to assist FPL fans to select the optimal starting line-up according to data collected from Gameweek 1 to the most recent gameweek of the 2023/24 season. 
             The data collected reflects the top 39 highest rated players so far this season in terms of FPL points accumulated over 23 game weeks. By leveraging D-Wave's LeapHybridSolver,
             you will be able to view what the optimal starting line-up would be based on your desired formation. You can sign up for D-Wave Leap [here](https://cloud.dwavesys.com/leap/) and obtain your solver API token. 
             Once you have obtained it, insert it into the D-Wave Solver API token in the sidebar.
@@ -26,7 +29,72 @@ st.write("""The purpose of this app is to assist FPL fans to select the optimal 
 #token_use = os.getenv("API_TOKEN")
 
 # Loading in the FPL data set
-data = pd.read_excel("data.xlsx")
+extract_data = pd.read_csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2023-24/gws/merged_gw.csv")
+
+def accumulated(df):
+    # Add a column 'n' with all values set to 1
+    df['n'] = 1
+    
+    # Group by 'name' and 'team'
+    grouped = df.groupby(['name', 'team'])
+    
+    # Define the columns to apply the cumulative sum and then subtract the original value
+    cols_to_accumulate = ['goals_scored', 'assists', 'ict_index', 'goals_conceded', 'minutes', 'own_goals', 'bps', 'clean_sheets', 'bonus']
+    
+    # Apply the cumulative sum and subtraction
+    for col in cols_to_accumulate:
+        df[col + '_cum'] = grouped[col].cumsum() - df[col]
+    
+    # For 'total_points', just the cumulative sum is needed
+    df['total_points_cum'] = grouped['total_points'].cumsum()
+    
+    # Select the desired columns
+    df = df[['name', 'team', 'position', 'value', 'goals_scored_cum', 'assists_cum', 'ict_index_cum', 'goals_conceded_cum', 'minutes_cum', 'own_goals_cum', 'total_points_cum', 'bps_cum', 'bonus_cum', 'clean_sheets_cum', 'GW', 'kickoff_time']]
+    
+    # Filter rows where 'ict_index_cum' > 0
+    #df = df[df['ict_index_cum'] > 0]
+    
+    # Drop the temporary 'n' column
+    #df.drop(columns=['n'], inplace=True)
+    
+    # Rename the accumulated columns back to their original names for clarity
+    df.rename(columns={col + '_cum': col for col in cols_to_accumulate}, inplace=True)
+    df.rename(columns={'total_points_cum': 'total_points'}, inplace=True)
+    
+    return df
+
+cumulative_df = accumulated(extract_data)
+
+cumulative_df["date"] = pd.to_datetime(cumulative_df["kickoff_time"]).dt.date
+cumulative_df.index = cumulative_df["date"]
+cumulative_df = cumulative_df.sort_index()
+date_range = pd.date_range(start=cumulative_df.index.min(), end = cumulative_df.index.max())
+cumulative_df["name_team"] = cumulative_df["name"] + "_" + cumulative_df["team"]
+new_df = pd.DataFrame()
+for combo in list(cumulative_df["name_team"].unique()):
+    new_data = cumulative_df[cumulative_df["name_team"] == combo]
+    post = new_data["position"].unique()[0]
+    name, team = combo.split("_")[0], combo.split("_")[1]
+    new_data_reindexed = new_data.reindex(date_range)
+    new_data_reindexed["name"] = name
+    new_data_reindexed["team"] = team
+    new_data_reindexed["position"] = post
+    new_data_reindexed["date"] = new_data_reindexed.index
+    new_data_reindexed = new_data_reindexed.ffill(axis=0)
+    new_data_reindexed = new_data_reindexed.dropna()
+    new_data_reindexed = new_data_reindexed.reset_index(drop=True)
+    
+    new_df = pd.concat([new_df, new_data_reindexed], axis = 0).reset_index(drop=True)
+    
+latest_df = new_df[new_df["date"]==max(new_df["date"])][["name", "team", "total_points", "date", "position", "value", "GW"]].reset_index(drop=True)
+
+gk = latest_df[latest_df["position"]=="GK"].sort_values("total_points", ascending=False).reset_index(drop=True).head(5)
+defenders = latest_df[latest_df["position"]=="DEF"].sort_values("total_points", ascending=False).reset_index(drop=True).head(10)
+midfielders = latest_df[latest_df["position"]=="MID"].sort_values("total_points", ascending=False).reset_index(drop=True).head(10)
+forwards = latest_df[latest_df["position"]=="FWD"].sort_values("total_points", ascending=False).reset_index(drop=True).head(10)
+data = pd.concat([gk, defenders, midfielders, forwards], axis = 0).sort_values("position", ascending=False).reset_index(drop=True)
+gw = max(data["GW"])
+
 columns = ["name", "position", "value", "total_points"]
 data = data[columns]
 data["value"] = data["value"]/10
@@ -100,26 +168,27 @@ else:
                             sampler = LeapHybridSampler(token= api_key)
                             sampleset = sampler.sample(bqm,
                                                         label="FPL line-up optimization")
-                            # Decode samples and select the best one
+                            
                             decoded_samples = model.decode_sampleset(sampleset)
                             best_sample = min(decoded_samples, key=lambda x: x.energy)
                             
-                            # Print to see if constraints are fulfilled
-                            print(best_sample.constraints())
-                            # Print results for best line-up
+                            
+                            #print(best_sample.constraints())
+                            
                             lineup_df = pd.DataFrame(best_sample.sample.items())
                             lineup_df.columns = ['variable', 'selected']
                             lineup_df = lineup_df[(lineup_df['variable'].str.startswith(
                                 'x', na=False)) & (lineup_df['selected'] == 1)]
                             lineup_df = df_use.merge(lineup_df, on=['variable'])
                             
-                            # Print line-up and maximized energy for the objective function
+                            
                             gk = lineup_df[lineup_df["position"] == "GK"]
                             defense_list = lineup_df[lineup_df["position"] == "DEF"]
                             midfield_list = lineup_df[lineup_df["position"] == "MID"]
                             forward_list = lineup_df[lineup_df["position"] == "FWD"]
                             ordered_lineup_df = pd.concat([gk, defense_list, midfield_list, forward_list], axis=0).reset_index(drop=True)
                             ordered_lineup_df = ordered_lineup_df[["name", "position", "value", "total_points"]]
+                            st.write("After game week ", gw, ", the optimal ", defense, "-", midfield, "-", forward, "starting line-up is:")
                             st.dataframe(ordered_lineup_df)
                             st.write("Total sum of points: ", ordered_lineup_df['total_points'].sum())
                             st.write("Total budget: ", round(ordered_lineup_df['value'].sum(), 4))
