3f8abfe5abeda3edf2d32102a6ed6a4f035adffd
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2024-03-18 17:08:01 -0500

    Update FPL_Lineup_⚽.py

--- FPL_Lineup_⚽.py
+++ FPL_Lineup_⚽.py
@@ -162,9 +162,9 @@ def plot_formation(line_up):
     fwd_num = list(np.linspace(0, 100, fwd_len+2))[1:-1]
     for i in range(len(fwd_num)):
         ax.plot(150, fwd_num[i], 'o', markersize=30, color="purple", markeredgecolor="white")  # Player icon
-        ax.text(150, fwd_num[i] - 5, fwd_names[i], ha="center", va="top", color="white", fontsize=8, fontweight="bold")  # Player name
+        ax.text(150, fwd_num[i] - 5, fwd_names[i], ha="center", va="top", color="white", fontsize=7, fontweight="bold")  # Player name
         info = str(fwd_points[i]) + ", " + str(fwd_values[i])
-        ax.text(150, fwd_num[i] - 7, info, ha="center", va="top", color="white", fontsize=8, fontweight="bold")  
+        ax.text(150, fwd_num[i] - 7, info, ha="center", va="top", color="white", fontsize=7, fontweight="bold")  
 
     st.pyplot(fig)
 
